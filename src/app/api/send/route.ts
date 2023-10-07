a7ea3aa01d4278b58dc0fdf82e7ae71098d8e821
rum <rumeshranaweera99@gmail.com>
2023-10-07 14:16:38 +0530

    fix send/route req:Request

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { z } from "zod";
 import nodemailer from "nodemailer";
 
-export async function POST(req: Response) {
+export async function POST(req: Request) {
   const data: unknown = await req.json();
   try {
     const { email, message, name, subject } = emailSchema.parse(data);
