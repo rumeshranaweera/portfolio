2a5cea65e3f23448878ce08a36b5a6ff4a965e99
rum <rumeshranaweera99@gmail.com>
2023-10-07 22:38:22 +0530

    add stagger text

--- src/components/heroTitle.tsx
+++ src/components/heroTitle.tsx
@@ -4,15 +4,15 @@ import { AnimationProps, motion } from "framer-motion";
 
 const HeroTitle = () => {
   const letter: AnimationProps["variants"] = {
-    hidden: { y: -20 },
-    visible: { y: 0 },
+    hidden: { y: -20, opacity: 0.2 },
+    visible: { y: 0, opacity: 1 },
   };
 
   return (
     <motion.h1
       initial="hidden"
       animate="visible"
-      transition={{ staggerChildren: 0.1 }}
+      transition={{ staggerChildren: 0.02 }}
       className="text-5xl md:text-7xl text-center font-bold my-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-500"
     >
       <span className="sr-only">Rumesh Ranaweera</span>
