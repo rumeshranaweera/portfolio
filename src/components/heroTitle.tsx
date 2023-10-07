177820b770f6d83c0b7b28d78db545ce5ab4edb0
rum <rumeshranaweera99@gmail.com>
2023-10-07 21:49:10 +0530

    add title

---
+++ src/components/heroTitle.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { AnimationProps, motion } from "framer-motion";
+
+const HeroTitle = () => {
+  const letter: AnimationProps["variants"] = {
+    hidden: { y: -20 },
+    visible: { y: 0 },
+  };
+
+  return (
+    <motion.h1
+      initial="hidden"
+      animate="visible"
+      transition={{ staggerChildren: 0.1 }}
+      className="text-5xl md:text-7xl text-center font-bold my-10 bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-500"
+    >
+      <span className="sr-only">Rumesh Ranaweera</span>
+      {"Rumesh Ranaweera".split("").map((l, index) => (
+        <motion.span aria-hidden key={index} variants={letter}>
+          {l}
+        </motion.span>
+      ))}
+    </motion.h1>
+  );
+};
+
+export default HeroTitle;
