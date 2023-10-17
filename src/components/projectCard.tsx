c67040b4dcb9920b652f51279c438da9d1c22424
rum <rumeshranaweera99@gmail.com>
2023-10-17 23:42:18 +0530

    add hover animation to project cards

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,22 +1,46 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { MouseEvent } from "react";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import { VercelResponse } from "@/types/types";
-import Link from "next/link";
 export function ProjectCard({ data }: { data: VercelResponse["projects"][0] }) {
   //@ts-ignore
-  const projectUrl = data?.latestDeployments[0].alias[0] || "hello";
+  const projectUrl = data?.latestDeployments[0].alias[0];
+
+  let mouseX = useMotionValue(0);
+  let mouseY = useMotionValue(0);
+
+  function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+    let { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
 
   return (
-    <Card className="w-[350px]">
+    <Card
+      className="w-[350px] group relative max-w-md rounded-xl shadow-xl"
+      onMouseMove={handleMouseMove}
+    >
+      <motion.div
+        className="pointer-events-none absolute -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
+        style={{
+          background: useMotionTemplate`
+            radial-gradient(
+              650px circle at ${mouseX}px ${mouseY}px,
+              rgba(23, 23, 23, 0.15),
+              transparent 20%
+            )
+          `,
+        }}
+      />
       <CardHeader>
         <CardTitle>{data.name}</CardTitle>
         <CardDescription>
