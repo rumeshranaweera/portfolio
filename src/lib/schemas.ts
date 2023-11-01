511eb218540e2fa3cd6b4ed9b49e0d56d374236f
rum <rumeshranaweera99@gmail.com>
2023-11-01 13:25:10 +0530

    change schema validation rules

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const emailSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }).max(50),
+  name: z.string().min(3, { message: "Name is required" }).max(50),
   email: z.string().email("Invalid email address"),
   subject: z.string().min(5, { message: "min length is 5" }).max(150),
-  message: z.string().min(1, { message: "min length is 10" }).max(1000),
+  message: z.string().min(10, { message: "min length is 10" }).max(1000),
 });
