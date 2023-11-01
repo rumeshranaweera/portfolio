2f332e5b9fbfa053756ceb4589547943fd5b4d66
rum <rumeshranaweera99@gmail.com>
2023-11-01 13:38:48 +0530

    add error massages

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 export const emailSchema = z.object({
-  name: z.string().min(3, { message: "Name is required" }).max(50),
+  name: z
+    .string()
+    .min(3, { message: "Must be 3 or more characters long" })
+    .max(50, { message: "Less than 50 characters" }),
   email: z.string().email("Invalid email address"),
   subject: z.string().min(5, { message: "min length is 5" }).max(150),
   message: z.string().min(10, { message: "min length is 10" }).max(1000),
